d372c9d4bf0c237ee3d59890b4f4ab528fa10ace
Wolfgang Hafenscher <wolfgang.hafenscher@kpibench.com>
2018-11-29 11:02:10 +0100

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # php-humanize-json
+Custom PHP function to pretty print json strings / json objects for easier human readibility.
+
+Incorporates code from php.net user comment: http://php.net/manual/en/function.highlight-string.php#118550
---
+++ sample.php
@@ -0,0 +1,37 @@
+<?php
+
+    // json pretty format function
+	// incorporates code taken from php manual comment at http://php.net/manual/en/function.highlight-string.php#118550
+    function prettyJson($jsonObj)
+    {
+		// create jsonObject from string (if given param is string)
+		if('string' === gettype($jsonObj)) {
+			$jsonObj = json_decode($jsonObj);
+		}
+
+		$text = json_encode($jsonObj, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+        $text = trim($text);
+        $text = highlight_string("<?php " . $text, true);  // highlight_string() requires opening PHP tag or otherwise it will not colorize the text
+        $text = trim($text);
+        $text = preg_replace("|^\\<code\\>\\<span style\\=\"color\\: #[a-fA-F0-9]{0,6}\"\\>|", "", $text, 1);  // remove prefix
+        $text = preg_replace("|\\</code\\>\$|", "", $text, 1);  // remove suffix 1
+        $text = trim($text);  // remove line breaks
+        $text = preg_replace("|\\</span\\>\$|", "", $text, 1);  // remove suffix 2
+        $text = trim($text);  // remove line breaks
+        $text = preg_replace("|^(\\<span style\\=\"color\\: #[a-fA-F0-9]{0,6}\"\\>)(&lt;\\?php&nbsp;)(.*?)(\\</span\\>)|", "\$1\$3\$4", $text);  // remove custom added "<?php "
+
+        // color format property names
+        $pattern = "|\\<span style\\=\"color\\: #[a-fA-F0-9]{0,6}\"\\>\"([^\"]+)\"\\</span\\>\\<span style\\=\"color\\: (#[a-fA-F0-9]{0,6})\"\\>\\:|";
+        $replace = '<span style="color: #333333;">\1</span><span style="color: \2">:';
+        $text = preg_replace($pattern, $replace, $text);
+
+        return $text;
+    }
+
+
+    $sampleJson = '{"a": 200, "b": "sample", "c": {"p1": "test", "p2": "test2"}, "d": [0, 23, 42]}';
+    echo '<pre>';
+    echo prettyJson($sampleJson);
+    echo '</pre>';
+
